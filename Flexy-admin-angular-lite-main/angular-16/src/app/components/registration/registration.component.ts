2b6d20f46b0cf1763e37f0d536517083dcb99d76
RC <rutuchoudhari22@gmail.com>
2024-07-17 12:05:21 +0530

    login page added

--- Flexy-admin-angular-lite-main/angular-16/src/app/components/registration/registration.component.ts
+++ Flexy-admin-angular-lite-main/angular-16/src/app/components/registration/registration.component.ts
@@ -15,46 +15,46 @@ import { Router } from '@angular/router';
   templateUrl: './registration.component.html',
 })
 export class RegistrationComponent  {
-onSubmit() {
-throw new Error('Method not implemented.');
-}
-checked: BooleanInput;
-registrationForm: FormGroup<any> | undefined;
-  forms: FormGroup | undefined;
+// onSubmit() {
+// throw new Error('Method not implemented.');
+// }
+// checked: BooleanInput;
+// registrationForm: FormGroup<any> | undefined;
+//   forms: FormGroup | undefined;
 
-  constructor(private formBuilder: FormBuilder) { }
+//   constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
-    this.forms = this.formBuilder.group({
-      employeeName: ['', Validators.required],
-      employeeId: [''],
-      birthdate: ['', Validators.required],
-      mobileNumber: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      address: ['', Validators.required],
-      gender: ['']
-    });
-  }
+//   ngOnInit(): void {
+//     this.forms = this.formBuilder.group({
+//       employeeName: ['', Validators.required],
+//       employeeId: [''],
+//       birthdate: ['', Validators.required],
+//       mobileNumber: ['', Validators.required],
+//       email: ['', [Validators.required, Validators.email]],
+//       address: ['', Validators.required],
+//       gender: ['']
+//     });
+//   }
 
-  onSubmit() {
-    if (this.forms.valid) {
-      console.log(this.forms.value); // Replace with your form submission logic
-    } else {
-      // Mark all fields as touched to display validation messages
-      this.markFormGroupTouched(this.forms);
-    }
-  }
+//   onSubmit() {
+//     if (this.forms.valid) {
+//       console.log(this.forms.value); // Replace with your form submission logic
+//     } else {
+//       // Mark all fields as touched to display validation messages
+//       this.markFormGroupTouched(this.forms);
+//     }
+//   }
 
-  // Helper method to mark all fields as touched
-  private markFormGroupTouched(formGroup: FormGroup) {
-    Object.values(formGroup.controls).forEach(control => {
-      control.markAsTouched();
+//   // Helper method to mark all fields as touched
+//   private markFormGroupTouched(formGroup: FormGroup) {
+//     Object.values(formGroup.controls).forEach(control => {
+//       control.markAsTouched();
 
-      if (control instanceof FormGroup) {
-        this.markFormGroupTouched(control);
-      }
-    });
-  }
+//       if (control instanceof FormGroup) {
+//         this.markFormGroupTouched(control);
+//       }
+//     });
+//   }
 
 }
 
